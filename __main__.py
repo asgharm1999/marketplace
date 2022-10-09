291dd4304aab7a08479dd6873bb57784738b110c
Muhammad Asghar <asgharm1999@gmail.com>
2022-10-09 15:10:03 -0400

    working etsy

--- __main__.py
+++ __main__.py
@@ -6,35 +6,45 @@ import csv
 import urllib3
 import datetime
 import glob
-import aptdeco_scraper as aptd
-import uhaul_scraper as uhaul
-import craiglist_scraper as craigl
-import realsimple_scraper as real
+# import aptdeco_scraper as aptd
+# import uhaul_scraper as uhaul
+# import craiglist_scraper as craigl
+# import realsimple_scraper as real
 import etsy_scraper as etsy
-import yellowpages_scraper as yellow
-from apify_client import ApifyClient
+# import yellowpages_scraper as yellow
+# from apify_client import ApifyClient
 
-def import_or_install(package):
-    try:
-        __import__(package)
-    except:
-        pip.main(['install', package])       
 
-# Required for RealSimple
-packages = ['apify', 'apify-client']
-for p in packages:
-    import_or_install(p)
+
+
+etsy_base_url = "https://www.etsy.com/search?q=furniture&page={page}&ref=pagination"
+
+etsy.get_etsy_search_results(etsy_base_url)
+
+
+
+
+# def import_or_install(package):
+#     try:
+#         __import__(package)
+#     except:
+#         pip.main(['install', package])       
+
+# # Required for RealSimple
+# packages = ['apify', 'apify-client']
+# for p in packages:
+#     import_or_install(p)
 
 
 
 
 # craigslist_base_url = "https://pittsburgh.craigslist.org/search/fua"
-realsimple_base_url = "https://www.realsimple.com/home-organizing/decorating"
+# realsimple_base_url = "https://www.realsimple.com/home-organizing/decorating"
 
 
-token = "apify_api_Q8Nb6XkPh9ZdZnhkYCZL7BgKB5ZZF531g2xb"
-# Initialize the ApifyClient with your API token
-real.get_realsimple_search_results(realsimple_base_url, token)
+# token = "apify_api_Q8Nb6XkPh9ZdZnhkYCZL7BgKB5ZZF531g2xb"
+# # Initialize the ApifyClient with your API token
+# real.get_realsimple_search_results(realsimple_base_url, token)
 
 
 
--- etsy_scraper.py
+++ etsy_scraper.py
@@ -1,87 +1,34 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[228]:
-
-
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
+import datetime
 
+def get_etsy_search_results(base_url):
 
-# In[229]:
+    search_results = []
+    page = 1
+    titles = []
 
-
-fout = open('Etsy.txt', 'wt',encoding='utf-8')
-
-
-# In[230]:
-
-
-fclean = open('Etsy_clean.txt', 'wt')
-
-
-# In[271]:
-
-
-search_results = []
-page = 1
-titles = []
-
-while page < 3:
-    url = f"https://www.etsy.com/search?q=dining+table+and+chairs&ref=auto-1&as_prefix=dining+table+and+chairs&page={page}"
-    response=requests.get(url,headers=headers)
-    soup=BeautifulSoup(response.content,'html')
-    fout.write(str(soup))
-    for item in soup.select('.wt-grid__item-xs-6'): 
-        try: 
-            print('-------------------------')
-            #print(item)
-            title = item.select('h3')[0].get_text().strip()
-            print(title)
-            fclean.write('title')
-            price=item.select('.currency-value')[0].get_text().strip()
-            print(price)
-            fclean.write(price)
-            rating=item.find("input", {'name': "rating"}).attrs['value']
-            print(rating)
-            fclean.write(rating)
-            link = item.find("a", {'class': "listing-link wt-display-inline-block bbc3092379125a8bb"}).attrs['href']
-            if(link is None):
-                link = 'Missing'
-            print(link)
-            search_results.append([title, price, rating,link])
-        except Exception as e: 
+    while page < 30:
+        response = requests.get(base_url)
+        soup = BeautifulSoup(response.content,'html')
+        for item in soup.select('.wt-grid__item-xs-6'): 
+            try: 
+                title = item.select('h3')[0].get_text().strip()
+                price = item.select('.currency-value')[0].get_text().strip()
+                rating = item.find("input", {'name': "rating"}).attrs['value']
+                search_results.append([title, price, rating])
+            except Exception as e: 
                 #raise e 
-                b=0  
-    page = page+1
+                b = 0  
+        page = page + 1
     
 
-columns = ('Title', 'Price', 'Rating','Link')
-df = pd.DataFrame(search_results, columns=columns)
+    columns = ('Title', 'Price', 'Rating')
+    df = pd.DataFrame(search_results, columns=columns)
 
+    timestamp = datetime.datetime.now().strftime('%m_%d_%y %H%M%S')
+    df.to_csv(f'Etsy Results ({timestamp}).csv', index=False)
 
-# In[272]:
-
-
-df
-
-
-# In[254]:
-
-
-df.to_csv('Etsy_clean.csv')
-
-
-# In[226]:
-
-
-fout.close()
-fclean.close()
-
-
-# In[ ]:
-
-
-
-
+    # fout.close()
+    # fclean.close()
