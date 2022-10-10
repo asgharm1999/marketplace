39bdfd2518b8fa35405be68828f89fe0da56b4b6
Edvin William Handoko <edvin.wh@gmail.com>
2022-10-10 17:37:30 -0400

    furniest_edvin 221010:1737
    Copy craiglist_scraper.py, create_master_csv.py and get_cached_data.py from asghar

--- craiglist_scraper.py
+++ craiglist_scraper.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import pandas as pd
 import numpy as np
 import datetime
@@ -15,46 +9,67 @@ import re
 import os
 import math
 
+def get_craigslist_search_results(base_url):
+    page = requests.get(base_url)
 
-# In[2]:
+    # print(page.content)
 
+    soup = BeautifulSoup(page.content, "html.parser")
 
-base_url = "https://pittsburgh.craigslist.org/search/fua"
+    total_results = int(soup.find('span', 'totalcount').text)
+    # print(total_results)
 
-page = requests.get(base_url)
+    total_pages = (total_results // 120) + 1
 
+    search_results = []
 
-# In[3]:
+    results = soup.find('ul', {'id': 'search-results'})
+    results_row = results.find_all('li', 'result-row')
 
+    # print(results.prettify())
 
-print(page.content)
+    """
+    This code only scraps the first 5 pages. If you want to scrap all pages you can comment line 36 and instead run line 38.
+    This will take a long time.
+    """
+    for i in range(0, 6):
+    # for i in total_pages:
+        
+        params = {
+            's': i * 120        # 120 items per page
+        }
 
+        response = requests.get(base_url, params=params)
+        print('Processing Page {0}'.format(i+1))
 
-# In[4]:
+        response = requests.get(base_url, params=params)
+        soup = BeautifulSoup(response.content, 'html.parser')
 
+        results = soup.find('ul', {'id': 'search-results'})
+        result_rows = results.find_all('li', 'result-row')
 
-soup = BeautifulSoup(page.content, "html.parser")
+        for result_row in results_row:
+            post_datetime = result_row.time['datetime']
+            post_id = result_row.h3.a['data-id']
+            post_url = result_row.h3.a['href']
+            price = result_row.find('span', 'result-price').text
+            location = result_row.find('span', 'result-hood').text if result_row.find('span', 'result-hood') else ''
+            post_title = result_row.h3.a.text
+        # post_title = furniture_elem.find('a', class_="result-title hdrlnk")
 
-total_results = int(soup.find('span', 'totalcount').text)
-print(total_results)
+            search_results.append([
+                post_datetime, post_id, post_url, price, location, post_title
+            ])
 
-total_pages = (total_results // 120) + 1
+            time.sleep(1)
 
-search_results = []
+        columns = ('Post Date', 'Post Id', 'Post URL', 'Price', 'Location', 'Post Title')
+        df = pd.DataFrame(search_results, columns=columns)
 
+    # print(df)
 
-# In[5]:
-
-
-# results_row = soup.find(class_="rows")
-
-results = soup.find('ul', {'id': 'search-results'})
-results_row = results.find_all('li', 'result-row')
-
-print(results.prettify())
-
-
-# In[6]:
+    timestamp = datetime.datetime.now().strftime('%m_%d_%y %H%M%S')
+    df.to_csv(f'Craigslist Results ({timestamp}).csv', index=False)
 
 
 # furniture_elems = results.find_all('li', class_="result-row")
@@ -63,38 +78,7 @@ print(results.prettify())
 #     url = furniture_elem.find('a', class_="result-image gallery")['href']
 
 # changed to process 5 pages instead of total_pages
-for i in range(0, 6):
-    
-    params = {
-        's': i*120        
-    }
 
-    response = requests.get(base_url, params=params)
-    print('Processing Page {0}'.format(i+1))
-
-    response = requests.get(base_url, params=params)
-    soup = BeautifulSoup(response.content, 'html.parser')
-
-    results = soup.find('ul', {'id': 'search-results'})
-    result_rows = results.find_all('li', 'result-row')
-
-    for result_row in results_row:
-        post_datetime = result_row.time['datetime']
-        post_id = result_row.h3.a['data-id']
-        post_url = result_row.h3.a['href']
-        price = result_row.find('span', 'result-price').text
-        location = result_row.find('span', 'result-hood').text if result_row.find('span', 'result-hood') else ''
-        post_title = result_row.h3.a.text
-    # post_title = furniture_elem.find('a', class_="result-title hdrlnk")
-
-        search_results.append([
-            post_datetime, post_id, post_url, price, location, post_title
-        ])
-
-        time.sleep(1)
-
-    columns = ('Post Date', 'Post Id', 'Post URL', 'Price', 'Location', 'Post Title')
-    df = pd.DataFrame(search_results, columns=columns)
     
 #     print(price_elem.text.strip())
 #     print(location_elem)
@@ -104,34 +88,12 @@ for i in range(0, 6):
 #     print()
 
 
-# In[8]:
-
-
-print(df)
-
-
-# In[9]:
-
-
-timestamp = datetime.datetime.now().strftime('%m_%d_%y %H%M%S')
-df.to_csv(f'Craigslist Results ({timestamp}).csv', index=False)
-
-
-# In[10]:
-
-
 # FURNITURE_URL = 'https://pittsburgh.craigslist.org/fuo/d/murrysville-lift-recliner/7534553122.html'
 # furniture_page = requests.get(FURNITURE_URL)
 # furniture_soup = BeautifulSoup(furniture_page.content, 'html.parser')
 
 
-# In[11]:
-
-
 # attributes = furniture_soup.find_all('p', class_='attrgroup')
-
-
-# In[12]:
 
 
 # for attribute in attributes:
@@ -139,9 +101,6 @@ df.to_csv(f'Craigslist Results ({timestamp}).csv', index=False)
 #     for span in spans:
 #         text = span.text.strip()
 #         print(text)
-
-
-# In[13]:
 
 
 # posting_body = furniture_soup.find('section', {"id":"postingbody"})
